1b2048318bb1532a0cf659baa95a17a1e4d0bf0c
williamparker <baiweilian@gmail.com>
2020-02-18 09:41:13 -0600

    Various modifications to improve functionality

--- SquareWellMatrixEigenproblem.py
+++ SquareWellMatrixEigenproblem.py
@@ -1,22 +1,25 @@
 import numpy as np
 from numpy import linalg as LA
-import matplotlib as plt
-N = 10
+import matplotlib.pyplot as plt
+N = 5
 b = np.ones(N - 1)
 e = np.full(N, 2.)
 
-#print(b)
+print(b)
 
 a = np.diagflat(-b, 1)
+print(a)
 c = np.diagflat(-b, -1)
+print(c)
 d = np.diagflat(e)
-
+print(d)
 print(a + c + d)
-#ħ = 1
-#m = 1
-#Δx = (a - 0)/(N - 1)
-#print(Δx)
-#λ =  ħ**2/(2*m*(Δx)**2)
+hbar = 1
+mass = 1
+deltax = (1 - 0)/(N - 1)
+print(deltax)
+lamb =  hbar**2/(2*mass*(deltax)**2)
+print(lamb)
 #numerator = ħ**2
 #denom = 2*m*(Δx)**2
 #λ = np.divide(numerator, denom, out=np.zeros_like(numerator), where=denom!=0)
@@ -24,13 +27,19 @@ print(a + c + d)
 
 #print(λ*(a + c + d))
 
-eigenvalue , eigenvector = LA.eig(a + c + d)
+eigenvalue , eigenvector = LA.eig(lamb*(a + c + d))
 #print(eigenvalue)
 
 print()
 
+print(eigenvalue)
 first_three = eigenvalue[eigenvalue.argsort()[:3]]
+print(eigenvalue.argsort())
+print(eigenvalue[eigenvalue.argsort()])
 print(first_three)
+#print(eigenvalue)
+energy_one = np.power(np.pi, 2)/2.0
+print(energy_one)
 
 out = []
 
@@ -39,5 +48,6 @@ for elm in first_three:
 
 out = np.array(out)
 
-plt.pyplot.plot(out)
-plt.pyplot.show()
+#plt.plot(out)
+#plt.show()
+
